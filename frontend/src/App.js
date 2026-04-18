06d02b41aadebbced98a8e5a454878aa49d035f9
3942457246@qq.com <3942457246@qq.com>
2026-04-18 21:38:42 +0800

    添加了项目基础机构，并且设置了3处gitignore,修改了2处ci

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>AI 协作项目前端</h1>
+        <p>团队名称：__________</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
